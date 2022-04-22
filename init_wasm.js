ba442dc9aa797fe82090c4f9639cf72cc0eabb5f
Nicolas Choquet <nicolachoquet06250@gmail.com>
2022-04-22 10:08:04 +0000

    prise en charge du fonctionnement dans un sous répertoire

--- init_wasm.js
+++ init_wasm.js
@@ -1,6 +1,6 @@
 const go = new Go();
 
-WebAssembly.instantiateStreaming(fetch("/wasm/testnum.wasm"), go.importObject)
+WebAssembly.instantiateStreaming(fetch("wasm/testnum.wasm"), go.importObject)
     .then(async result => {
         await go.run(result.instance);
     })
