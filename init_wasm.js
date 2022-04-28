2a5568e29fbba1ee09ef1c6f4ee149a3477a9aca
Nicolas Choquet <nicolachoquet06250@gmail.com>
2022-04-28 10:37:44 +0200

    suppression du fichier executable du repo + ajout d'un job de déploy de ce fichier sur github gist à la place

--- init_wasm.js
+++ init_wasm.js
@@ -1,6 +1,13 @@
 const go = new Go();
 
-WebAssembly.instantiateStreaming(fetch("wasm/testnum.wasm"), go.importObject)
+const gist_url = 'https://gist.githubusercontent.com/nicolachoquet06250/2f001ee83be8a23e0ec605ec3a9de016';
+
+const filename = 'testnum.wasm';
+
+const wasm_file_path = window.location.host.indexOf('.github.io') !== -1
+    ? `${gist_url}/raw/0cc8148a2b5afead6a003656c4cae8b19ef43ce9/${filename}` : `wasm/${filename}`;
+
+WebAssembly.instantiateStreaming(fetch(wasm_file_path), go.importObject)
     .then(async result => {
         await go.run(result.instance);
     })
