900d34b44f4a82f427eb85194c5c5818ea85767a
Nicolas Choquet <nicolachoquet06250@gmail.com>
2022-04-28 13:57:56 +0200

    tests github CI

--- init_wasm.js
+++ init_wasm.js
@@ -5,7 +5,7 @@ const gist_url = 'https://gist.githubusercontent.com/nicolachoquet06250/2f001ee8
 const filename = 'testnum.wasm';
 
 const wasm_file_path = window.location.host.indexOf('.github.io') !== -1
-    ? `${gist_url}/raw/0cc8148a2b5afead6a003656c4cae8b19ef43ce9/${filename}` : `wasm/${filename}`;
+    ? `${gist_url}/raw/1f067358c02bde72a12b607598b5e5d817067a4e/${filename}` : `wasm/${filename}`;
 
 WebAssembly.instantiateStreaming(fetch(wasm_file_path), go.importObject)
     .then(async result => {
